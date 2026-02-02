140a34bcf94e3b648ed6a340736effc83a6ae069
Dam <dfnex10@gmail.com>
2026-02-02 12:15:46 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,16 +3,11 @@ const pulls = [
   { name: "Uncommon", chance: 25 },
   { name: "Rare", chance: 15 },
   { name: "Epic", chance: 5 },
-  { name: "Legendary", chance: 3.5 },
-  { name: "Mythic", chance: 1.5 },
+  { name: "Legendary", chance: 3 },
+  { name: "Mythic", chance: 2 },
 ];
 
 //array stuff
-
-let pity = 0;
-const softpity = 20;
-const hardpity = 30;
-
 
 function getNumb(max) {
   return Math.floor(Math.random() * max);
@@ -23,12 +18,31 @@ function rolling() {
   if (x >= 1 && x <= 50) {
     console.log("Common");
   }; 
+  if (x >= 51 && x <= 75) {
+    console.log("Uncommon");
+  };
+  if (x >= 76 && x <= 90) {
+    console.log("Rare");
+  };
+  if (x >= 91 && x <= 95) {
+    console.log("Epic");
+  };
+  if (x >= 96 && x <= 98) {
+    console.log("Legendary");
+  };
+  if (x >= 99 && x <= 100) {
+    console.log("Mythic");
+  };
 };
 
 rolling();
-  
 
 //Have a random number generator and if the number lands bewteen 1-50, have it display common. If 51-75, display uncommon. If 76-90, rare. ETC...
 //FOR CHANCES
+
+let pity = 0;
+const guaranteedpity = 10;
+
+function pitycounter () {}
 
 //Pity system (Hard/Soft)
